b920e72106b6729680ff706ce4aee697b17b65f4
Lance Endres <lanceendres@gmail.com>
2023-03-17 08:07:43 -0500

    Better variable name.

--- src/Python/ddosi/drilling/data/PreProcessing.py
+++ src/Python/ddosi/drilling/data/PreProcessing.py
@@ -30,18 +30,18 @@ class PreProcessing():
         depth = data[depthColumn]
 
         # Insert a zero as a temporary place holder.
-        rop   = [0]
+        rateOfPenetration   = [0]
 
         for i in range(1, len(time)):
-            rop.append((depth[i] - depth[i-1]) / (time[i] - time[i-1]))
+            rateOfPenetration.append((depth[i] - depth[i-1]) / (time[i] - time[i-1]))
 
         # To calculate ROP, we need intervals of distance and time so we end up with one less entry for
         # ROP than either of those data sets.  The first entry is back filled with the same value as the
         # second entry to make them the same length.  Repeating an entry is better than adding a zero because
         # plots that use a starting value of zero create a big spike and look funny.
-        rop[0] = rop[1]
+        rateOfPenetration[0] = rateOfPenetration[1]
 
-        data["ROP"] = rop
+        data["ROP"] = rateOfPenetration
 
 
     @classmethod
