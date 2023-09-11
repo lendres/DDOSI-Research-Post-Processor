213cfc7babff75d46f8516ad35dcd00aa7295c8d
lendres <lanceendres@gmail.com>
2023-09-11 11:47:51 -0500

    New naming format, add RootMeanSquare

--- src/Python/ddosi/signalprocessing/SignalProcessing.py
+++ src/Python/ddosi/signalprocessing/SignalProcessing.py
@@ -10,7 +10,7 @@ class SignalProcessing():
     """
 
     @classmethod
-    def CreateMovingAverageOfData(cls, data, columns, numberOfPoints):
+    def MovingAverage(cls, data, columns, numberOfPoints):
         """
         Creates moving averages for each column specified in "columns".  The moving averae is added to the DataFrame as a new
         column.  The new column name is the original column name with a suffix added that indicates the number of points used.
@@ -28,8 +28,12 @@ class SignalProcessing():
         -------
         newNames : list of strings
             A list of the new column names.
+        suffix : str
+            The suffix added to the column names.
         """
         newNames = []
+        suffix   = str(numberOfPoints) + " pt Moving Average"
+
         for column in columns:
             movingAverage    = data[column].rolling(numberOfPoints).mean()
 
@@ -38,14 +42,53 @@ class SignalProcessing():
             # the NaNs are replaced with the first valid value.
             movingAverage.fillna(movingAverage[numberOfPoints], inplace=True)
 
-            columnName       = column + " " + str(numberOfPoints) + " pt Moving Average"
+            columnName       = column + " " + suffix
             data[columnName] = movingAverage
             newNames.append(columnName)
-        return newNames
+        return newNames, suffix
 
 
     @classmethod
-    def CreateLowPlassFilteredData(cls, data, columns, cutOff, samplingFrequency, order=2):
+    def RootMeanSquare(cls, data, columns, numberOfPoints):
+        """
+        Creates a root mean square for each column specified in "columns".  The root mean square is added to the DataFrame as a new
+        column.  The new column name is the original column name with a suffix added that indicates the number of points used.
+
+        Parameters
+        ----------
+        data : pandas.DataFrame
+            Data in a pandas.DataFrame
+        columns : array like of strings
+            The column names in a list.
+        numberOfPoints : int
+            The number of points to use for the moving average.
+
+        Returns
+        -------
+        newNames : list of strings
+            A list of the new column names.
+        suffix : str
+            The suffix added to the column names.
+        """
+        newNames = []
+        suffix   = str(numberOfPoints) + " pt RMS"
+
+        for column in columns:
+            rootMeanSquare  = data[column].pow(2).rolling(numberOfPoints).mean().pow(0.5)
+
+            # The front of the series gets filled with NaN until "numberOfPoints" entry is reached (that many points are needed
+            # to start the moving avaerage calculation).  Because, NaNs cause a lot of problems in calculations/plotting/et cetera,
+            # the NaNs are replaced with the first valid value.
+            rootMeanSquare.fillna(rootMeanSquare[numberOfPoints], inplace=True)
+
+            columnName       = column + " " + suffix
+            data[columnName] = rootMeanSquare
+            newNames.append(columnName)
+        return newNames, suffix
+
+
+    @classmethod
+    def LowPlassFilter(cls, data, columns, cutOff, samplingFrequency, order=2):
         """
         Creeates low pass output for each column specified in "columns".  The low pass data is added to the DataFrame as a new
         column.  The new column name is the original column name with a suffix added that indicates the cut off frequency.
@@ -67,11 +110,15 @@ class SignalProcessing():
         -------
         newNames : list of strings
             A list of the new column names.
+        suffix : str
+            The suffix added to the column names.
         """
         newNames = []
+        suffix   = str(cutOff) + " Hz Lowpass Filtered"
+
         for column in columns:
             filteredData, b, a = ButterworthLowPassFilter(data[column], cutOff, samplingFrequency, order)
-            columnName         = column + " " + str(cutOff) + " Hz Lowpass Filtered"
+            columnName         = column + " " + suffix
             data[columnName]   = filteredData
             newNames.append(columnName)
-        return newNames
+        return newNames, suffix
