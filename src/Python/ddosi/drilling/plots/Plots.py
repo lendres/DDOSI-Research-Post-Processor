60cbc672d39f3f86f6efc6e0edfc32846a0423ee
lendres <lanceendres@gmail.com>
2023-09-11 11:47:30 -0500

    Add type hints

--- src/Python/ddosi/drilling/plots/Plots.py
+++ src/Python/ddosi/drilling/plots/Plots.py
@@ -2,6 +2,7 @@
 Created on March 15, 2023
 @author: Lance A. Endres
 """
+import pandas                                                        as pd
 from   lendres.plotting.PlotHelper                                   import PlotHelper
 from   lendres.plotting.PlotMaker                                    import PlotMaker
 from   lendres.plotting.AxesHelper                                   import AxesHelper
@@ -10,7 +11,7 @@ class Plots():
 
 
     @classmethod
-    def NewWobAndRpmPlot(cls, title, data, xAxisColumn="Time", wobColumn="WOB", xUnits="s", rpmColumn="RPM", **kwargs):
+    def NewWobAndRpmPlot(cls, title:str, data:pd.DataFrame, xAxisColumn:str="Time", wobColumn:str="WOB", xUnits:str="s", rpmColumn:str="RPM", **kwargs):
         figure, axes = PlotMaker.NewMultiYAxesPlot(data, xAxisColumn, [[wobColumn], [rpmColumn]], **kwargs)
 
         # Labels.
@@ -21,7 +22,7 @@ class Plots():
 
 
     @classmethod
-    def NewDepthBasedWobAndRpmPlot(cls, title, data, yAxisColumn="Depth", wobColumn="WOB", yUnits="cm", rpmColumn="RPM", **kwargs):
+    def NewDepthBasedWobAndRpmPlot(cls, title:str, data:pd.DataFrame, yAxisColumn:str="Depth", wobColumn:str="WOB", yUnits:str="cm", rpmColumn:str="RPM", **kwargs):
         PlotHelper.scale      = 0.4
         PlotHelper.widthScale = 0.35
         figure, axes = PlotMaker.NewMultiXAxesPlot(data, yAxisColumn, [[wobColumn], [rpmColumn]], **kwargs)
@@ -38,22 +39,22 @@ class Plots():
 
 
     @classmethod
-    def NewTobWobAndRpmPlot(cls, title, data, xAxisColumn="Time", tobColumn="TOB", wobColumn="WOB", rpmColumn="RPM", **kwargs):
+    def NewTobWobAndRpmPlot(cls, title:str, data:pd.DataFrame, xAxisColumn:str="Time", tobColumn:str="TOB", wobColumn:str="WOB", rpmColumn:str="RPM", **kwargs):
         return cls.NewParameterVersusWobAndRpmPlot(title, data, xAxisColumn, "Torque (daN.m)", tobColumn, wobColumn, rpmColumn, **kwargs)
 
 
     @classmethod
-    def NewRopWobAndRpmPlot(cls, title, data, xAxisColumn="Time", ropColumn="ROP", wobColumn="WOB", rpmColumn="RPM", **kwargs):
+    def NewRopWobAndRpmPlot(cls, title:str, data:pd.DataFrame, xAxisColumn:str="Time", ropColumn:str="ROP", wobColumn:str="WOB", rpmColumn:str="RPM", **kwargs):
         return cls.NewParameterVersusWobAndRpmPlot(title, data, xAxisColumn, "Rate of Penetration (cm/s)", ropColumn, wobColumn, rpmColumn, **kwargs)
 
 
     @classmethod
-    def NewDepthOfCutWobAndRpmPlot(cls, title, data, xAxisColumn="Time", depthOfCutColumn="Depth of Cut", wobColumn="WOB", rpmColumn="RPM", **kwargs):
+    def NewDepthOfCutWobAndRpmPlot(cls, title:str, data:pd.DataFrame, xAxisColumn:str="Time", depthOfCutColumn:str="Depth of Cut", wobColumn:str="WOB", rpmColumn:str="RPM", **kwargs):
         return cls.NewParameterVersusWobAndRpmPlot(title, data, xAxisColumn, "Depth of Cut (cm/revolution)", depthOfCutColumn, wobColumn, rpmColumn, **kwargs)
 
 
     @classmethod
-    def NewParameterVersusWobAndRpmPlot(cls, title, data, xAxisColumn, parameterLabel, parameterColumn, wobColumn="WOB", rpmColumn="RPM", **kwargs):
+    def NewParameterVersusWobAndRpmPlot(cls, title:str, data:pd.DataFrame, xAxisColumn:str, parameterLabel:str, parameterColumn:str, wobColumn:str="WOB", rpmColumn:str="RPM", **kwargs):
         figure, axes = PlotMaker.NewMultiYAxesPlot(data, xAxisColumn, [[parameterColumn], [wobColumn], [rpmColumn]], **kwargs)
 
         # Labels.
