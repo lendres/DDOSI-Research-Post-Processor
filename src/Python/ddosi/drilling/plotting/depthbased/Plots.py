606c5e056e7449dbc6b5ebada906f6537576aa15
lendres <lanceendres@gmail.com>
2023-09-26 18:04:44 -0500

    Update depth plots
    Add Create* function.
    Use AxisHelper labeling of multiple axeses.

--- src/Python/ddosi/drilling/plotting/depthbased/Plots.py
+++ src/Python/ddosi/drilling/plotting/depthbased/Plots.py
@@ -3,14 +3,26 @@ Created on March 15, 2023
 @author: Lance A. Endres
 """
 import pandas                                                        as pd
+import matplotlib.pyplot                                             as plt
+
 from   lendres.plotting.AxesHelper                                   import AxesHelper
 from   lendres.plotting.PlotHelper                                   import PlotHelper
 from   lendres.plotting.PlotMaker                                    import PlotMaker
+from   lendres.plotting.LegendHelper                                 import LegendHelper
+from   lendres.plotting.LegendOptions                                import LegendOptions
 
 from   ddosi.plotting.DesignatedColors                               import DesignatedColors
 
 
 class Plots():
+
+    
+    @classmethod
+    def CreateWobAndRotarySpeedPlot(cls, data:pd.DataFrame, yAxisColumn:str="Depth", yUnits:str="cm", wobColumn:str="Weight on Bit", rpmColumn:str="Rotary Speed", title:str="Weight on Bit and Rotary Speed", titleSuffix:str=None, legendOptions:LegendOptions=LegendOptions(), **kwargs):
+        figure, axeses = cls.NewWobAndRotarySpeedPlot(data, yAxisColumn, wobColumn, yUnits, rpmColumn, title, titleSuffix, **kwargs)
+        LegendHelper.CreateLegendAtFigureBottom(figure, axeses[0], offset=0.02*PlotHelper.scale, legendOptions=legendOptions)
+        plt.show()
+        return figure, axeses
 
 
     @classmethod
@@ -18,16 +30,15 @@ class Plots():
         PlotHelper.scale      = 0.4
         PlotHelper.widthScale = 0.35
         
-        yDataLabels = [[wobColumn], [rpmColumn]]
-        kwargs      = DesignatedColors.ApplyKeyWordArgumentsToColors(kwargs, yDataLabels)
-        figure, axes = PlotMaker.NewMultiXAxesPlot(data, yAxisColumn, yDataLabels, **kwargs)
+        yDataLabels    = [[wobColumn], [rpmColumn]]
+        kwargs         = DesignatedColors.ApplyKeyWordArgumentsToColors(kwargs, yDataLabels)
+        figure, axeses = PlotMaker.NewMultiXAxesPlot(data, yAxisColumn, yDataLabels, **kwargs)
 
         # Labels.
-        AxesHelper.Label(axes[0], title, "Weight on Bit (tons)", yAxisColumn+" ("+yUnits+")", titleSuffix=titleSuffix)
-        AxesHelper.ReverseYAxisLimits(axes[0])
-        axes[1].set_xlabel("Revolutions per Minute")
+        AxesHelper.Label(axeses, title, ["Weight on Bit (tons)", "Revolutions per Minute"], yAxisColumn+" ("+yUnits+")", titleSuffix=titleSuffix)
+        AxesHelper.ReverseYAxisLimits(axeses[0])
 
         PlotHelper.scale      = 0.8
         PlotHelper.widthScale = 1.0
 
-        return figure, axes
+        return figure, axeses
