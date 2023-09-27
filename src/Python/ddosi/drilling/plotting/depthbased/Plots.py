13c163c1eaf27bc7c03645087b7f330bc0f9a6f7
lendres <lanceendres@gmail.com>
2023-09-27 16:13:43 -0500

    Update DepthPlots, add comments.

--- src/Python/ddosi/drilling/plotting/depthbased/Plots.py
+++ src/Python/ddosi/drilling/plotting/depthbased/Plots.py
@@ -16,9 +16,51 @@ from   ddosi.plotting.DesignatedColors                               import Desi
 
 class Plots():
 
-    
+
     @classmethod
     def CreateWobAndRotarySpeedPlot(cls, data:pd.DataFrame, yAxisColumn:str="Depth", yUnits:str="cm", wobColumn:str="Weight on Bit", rpmColumn:str="Rotary Speed", title:str="Weight on Bit and Rotary Speed", titleSuffix:str=None, legendOptions:LegendOptions=LegendOptions(), **kwargs):
+        """
+        Creates a depth based weight on bit and rotary speed plot.  The weight on bit and rotary speed are plotted on their own axes.  If specified,
+        a legend is generated.  The plot is finalized.
+
+        This function uses DesignatedColors to provide default colors for the plotted lines.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            The data.
+        yAxisColumn : str, optional
+            The column name of the depth data to use as the y-axis values. The default is "Depth".
+        wobColumn : str, optional
+            The column name of the weight on bit data. The default is "Weight on Bit".
+        rpmColumn : str, optional
+            The column name of the rotary speed data. The default is "Rotary Speed".
+        title : str, optional
+            The figure title. The default is "Weight on Bit and Rotary Speed".
+        titleSuffix : str, optional
+            If specified, it is added as a second line immediately under "title". The default is None.
+        legendOptions : LegendOptions, optional
+            Options that specify if and how the legend is generated. The default is LegendOptions().
+        **kwargs : keyword arguments
+            These arguments are passed to the plot function.  Each keyword argument can be a single value or a list.  If it is
+            a single value, the same value is used for every call to plat.  If it is a list, the values are passed in order to
+            each series as it is plotted.
+            Example 1:
+                axesesColumnNames=['Column 1', 'Column 2'], linewidth=4
+            Result
+                The data in 'Column 1' and 'Column 2' are potted with a 'linewidth' of 4.
+            Example 2:
+                axesesColumnNames=['Column 1', ['Column 2', 'Column 3'], 'Column 4'], linewidth=[1, 2, 3, 4]
+            Result
+                The data in 'Column 1', 'Column 2', 'Column 3', and 'Column 4' are potted with a 'linewidth's of 1. 2. 3. and 4, respectively.
+
+        Returns
+        -------
+        figure : matplotlib.figure.Figure
+            The newly created figure.
+        axeses : tuple of matplotlib.axes.Axes
+            The axes of the plot.
+        """
         figure, axeses = cls.NewWobAndRotarySpeedPlot(data, yAxisColumn, wobColumn, yUnits, rpmColumn, title, titleSuffix, **kwargs)
         LegendHelper.CreateLegendAtFigureBottom(figure, axeses[0], offset=0.02*PlotHelper.scale, legendOptions=legendOptions)
         plt.show()
@@ -27,10 +69,56 @@ class Plots():
 
     @classmethod
     def NewWobAndRotarySpeedPlot(cls, data:pd.DataFrame, yAxisColumn:str="Depth", wobColumn:str="Weight on Bit", yUnits:str="cm", rpmColumn:str="Rotary Speed", title:str="Weight on Bit and Rotary Speed", titleSuffix:str=None, **kwargs):
-        PlotHelper.scale      = 0.4
-        PlotHelper.widthScale = 0.35
-        
+        """
+        Creates a depth based weight on bit and rotary speed plot.  The weight on bit and rotary speed are plotted on their own axes.  The plot is NOT finalized.
+
+        Use this function to continue plotting
+
+        This function uses DesignatedColors to provide default colors for the plotted lines.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            The data.
+        yAxisColumn : str, optional
+            The column name of the depth data to use as the y-axis values. The default is "Depth".
+        wobColumn : str, optional
+            The column name of the weight on bit data. The default is "Weight on Bit".
+        rpmColumn : str, optional
+            The column name of the rotary speed data. The default is "Rotary Speed".
+        title : str, optional
+            The figure title. The default is "Weight on Bit and Rotary Speed".
+        titleSuffix : str, optional
+            If specified, it is added as a second line immediately under "title". The default is None.
+        legendOptions : LegendOptions, optional
+            Options that specify if and how the legend is generated. The default is LegendOptions().
+        **kwargs : keyword arguments
+            These arguments are passed to the plot function.  Each keyword argument can be a single value or a list.  If it is
+            a single value, the same value is used for every call to plat.  If it is a list, the values are passed in order to
+            each series as it is plotted.
+            Example 1:
+                axesesColumnNames=['Column 1', 'Column 2'], linewidth=4
+            Result
+                The data in 'Column 1' and 'Column 2' are potted with a 'linewidth' of 4.
+            Example 2:
+                axesesColumnNames=['Column 1', ['Column 2', 'Column 3'], 'Column 4'], linewidth=[1, 2, 3, 4]
+            Result
+                The data in 'Column 1', 'Column 2', 'Column 3', and 'Column 4' are potted with a 'linewidth's of 1. 2. 3. and 4, respectively.
+
+        Returns
+        -------
+        figure : matplotlib.figure.Figure
+            The newly created figure.
+        axeses : tuple of matplotlib.axes.Axes
+            The axes of the plot.
+        """
+        savedScales = [PlotHelper.widthScale, PlotHelper.heightScale]
+
+        PlotHelper.widthScale  = 0.68
+        PlotHelper.heightScale = 2.0
+
         yDataLabels    = [[wobColumn], [rpmColumn]]
+
         kwargs         = DesignatedColors.ApplyKeyWordArgumentsToColors(kwargs, yDataLabels)
         figure, axeses = PlotMaker.NewMultiXAxesPlot(data, yAxisColumn, yDataLabels, **kwargs)
 
@@ -38,7 +126,7 @@ class Plots():
         AxesHelper.Label(axeses, title, ["Weight on Bit (tons)", "Revolutions per Minute"], yAxisColumn+" ("+yUnits+")", titleSuffix=titleSuffix)
         AxesHelper.ReverseYAxisLimits(axeses[0])
 
-        PlotHelper.scale      = 0.8
-        PlotHelper.widthScale = 1.0
+        PlotHelper.widthScale  = savedScales[0]
+        PlotHelper.heightScale = savedScales[1]
 
         return figure, axeses
