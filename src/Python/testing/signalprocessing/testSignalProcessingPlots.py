ec76c20335c50c7d349e76a674088ff628a0bd56
lendres <lanceendres@gmail.com>
2023-10-15 10:29:23 -0500

    Update testSignalProcessingPlots.py

--- src/Python/testing/signalprocessing/testSignalProcessingPlots.py
+++ src/Python/testing/signalprocessing/testSignalProcessingPlots.py
@@ -37,16 +37,24 @@ class testSignalProcessingPlots(unittest.TestCase):
         timeLength         = 10
         samplingFrequency  = 1024
         frequency          = 60
+        magnitude          = 10
+        limit              = magnitude+1
         secondaryFrequency = 10
         steps              = timeLength*samplingFrequency
+        zoomedSamples      = 512
 
-        dataFrame = FunctionGenerator.NoisySineWaveAsDataFrame(noiseScale=0.1, magnitude=10, frequency=frequency, startTime=startTime, timeLength=timeLength, steps=steps)
+        dataFrame = FunctionGenerator.NoisySineWaveAsDataFrame(noiseScale=0.1, magnitude=magnitude, frequency=frequency, startTime=startTime, timeLength=timeLength, steps=steps)
+
+        self.CreateSineWavePlot(dataFrame["x"].iloc[:zoomedSamples], dataFrame["y original"].iloc[:zoomedSamples], "Method 1 - "+str(frequency)+" Hz Sine Wave", limit=limit)
+        self.CreateSineWavePlot(dataFrame["x"].iloc[:zoomedSamples], dataFrame["noise"].iloc[:zoomedSamples], "Method 1 - Noise", limit=limit)
+
         x, y      = FunctionGenerator.SineWave(magnitude=2, frequency=secondaryFrequency, startTime=startTime, timeLength=timeLength, steps=steps)
 
         dataFrame["y"] = dataFrame["y"] + y
 
-        self.CreateSineWavePlot(dataFrame["x"], dataFrame["y"], "Method 1")
-        self.CreateSineWavePlot(dataFrame["x"].iloc[:256], dataFrame["y"].iloc[:256], "Method 1 Zoomed")
+        self.CreateSineWavePlot(x[:zoomedSamples], y[:zoomedSamples], "Method 1 - "+str(secondaryFrequency)+" Hz Sine Wave", limit=limit)
+        # self.CreateSineWavePlot(dataFrame["x"], dataFrame["y"], "Method 1 - Combined Signal", limit=limit)
+        self.CreateSineWavePlot(dataFrame["x"].iloc[:zoomedSamples], dataFrame["y"].iloc[:zoomedSamples], "Method 1 - Zoomed Combined Signal")
         SignalProcessingPlots.CreateSpectrogramPlot(dataFrame, column="y", samplingFrequency=samplingFrequency, maxFequency=90, titleSuffix="Method 1")
 
 
@@ -74,7 +82,7 @@ class testSignalProcessingPlots(unittest.TestCase):
 
 
     @classmethod
-    def CreateSineWavePlot(cls, x, y, title):
+    def CreateSineWavePlot(cls, x, y, title, limit=None):
         """
         An example plot that is a simple sine wave.
 
@@ -97,6 +105,9 @@ class testSignalProcessingPlots(unittest.TestCase):
 
         axes.plot(x, y, label="Sine Wave")
         axes.set(title=title, xlabel="Time", ylabel="Signal")
+
+        if limit is not None:
+            axes.set_ylim(-limit, limit)
 
         plt.show()
 
