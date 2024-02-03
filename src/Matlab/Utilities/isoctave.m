76d0360fc861d6d75d08e1469a76406c990c4921
lendres <lanceendres@gmail.com>
2024-02-03 15:03:25 -0600

    Delete isoctave.m

--- src/Matlab/Utilities/isoctave.m
+++
@@ -1,8 +0,0 @@
-function isOctave = isoctave()
-    %ISOCTAVE Returns true if the environment is Octave.  Returns false if another
-    #   environment is running (for example, Matlab).
-    #
-    %   ISOCTAVE = ISOCTAVE()
-
-    isOctave                = exist("OCTAVE_VERSION", "builtin") ~= 0;
-end
